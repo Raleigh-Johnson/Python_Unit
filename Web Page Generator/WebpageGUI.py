9a9f3527d7359a5fda8999605cc094681dafa76a
Raleigh-Johnson <r.aleigh.j55@gmail.com>
2021-08-16 14:21:56 -0500

    Uploaded with comments

--- Web Page Generator/WebpageGUI.py
+++ Web Page Generator/WebpageGUI.py
@@ -18,17 +18,15 @@ class ParentWindow(Frame):
 
         self.sourceEntry = Entry(self.master)
 
-        
+        #Label to add text 
         self.lblAddText = Label(self.master,text='Enter Text: ', font=("Helvetica",16),fg='black',bg='beige')
         self.lblAddText.grid(row=0,column=0, padx=(5,0),pady=(10,0),sticky = W )
-
+        #Text body to add custome text 
         self.txtBody = Entry(self.master)
         self.txtBody.grid(row=1,column=0,padx=(5,0),pady=(0,0),sticky = W)
-
-        
+        #Opens file with selected custome text
         self.btncreate = Button(self.master,text='Open Webpage',command=lambda:WebPageFunction.newBody(self), font=("Helvetica",16),fg='black',bg='beige')
         self.btncreate.grid(row=2,column=0,padx=(5,0),pady=(20,0),sticky = W)
-
 
 if __name__ == "__main__":
     root = tkinter.Tk()
